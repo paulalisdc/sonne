ef722e82b5b38003016a9c66f5dafcc112d175c3
Martina <martinasansoneok@gmail.com>
2025-06-16 15:20:02 -0300

    Agregada función agregarProducto y lógica del formulario

--- src/components/ProductForm.jsx
+++ src/components/ProductForm.jsx
@@ -2,13 +2,67 @@ import { useState } from "react";
 import "./ProductForm.css";
 
 const ProductForm = () => {
+
+  //martina
+  
+  const [formulario, setFormulario] = useState({
+    categoria: "",
+    producto: "",
+    cantidad: "",
+    precio: "",
+  });
+
+  const [productos, setProductos] = useState([]);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormulario({
+      ...formulario,
+      [name]: value,
+    });
+  };
+
+  const agregarProducto = (e) => {
+    e.preventDefault();
+
+    if (
+      formulario.categoria === "" ||
+      formulario.producto === "" ||
+      formulario.cantidad === "" ||
+      formulario.precio === ""
+    ) {
+      alert("Por favor completá todos los campos.");
+      return;
+    }
+
+    const nuevoProducto = {
+      ...formulario,
+      cantidad: Number(formulario.cantidad),
+      precio: Number(formulario.precio),
+    };
+
+    setProductos([...productos, nuevoProducto]);
+
+    // Limpia el formulario
+    setFormulario({
+      categoria: "",
+      producto: "",
+      cantidad: "",
+      precio: "",
+    });
+
+    console.log("Producto agregado:", nuevoProducto);
+  };
+  //martina
     return (
-      <form className="product-form">
+      <form className="product-form" onSubmit={agregarProducto}> 
         <h2>Agregar producto al stock</h2>
   
         <div className="mb-3">
           <label htmlFor="categoria" className="form-label">Categoría</label>
-          <select id="categoria" name="categoria" className="form-select">
+          <select id="categoria" name="categoria" className="form-select" value={formulario.categoria}
+        onChange={handleChange}>
+            
             <option value="">Seleccionar</option>
             <option value="Aros">Aros</option>
             <option value="Collares">Collares</option>
@@ -27,6 +81,8 @@ const ProductForm = () => {
             name="producto"
             className="form-control"
             placeholder="Ej: Collar IRIS"
+             value={formulario.producto}
+            onChange={handleChange}
           />
         </div>
   
@@ -38,6 +94,8 @@ const ProductForm = () => {
             name="cantidad"
             className="form-control"
             placeholder="Ej: 10"
+            value={formulario.cantidad}
+            onChange={handleChange}
           />
         </div>
   
@@ -49,6 +107,8 @@ const ProductForm = () => {
             name="precio"
             className="form-control"
             placeholder="Ej: 2500"
+            value={formulario.precio}
+            onChange={handleChange}
           />
         </div>
   
